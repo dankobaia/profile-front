64c9b980949031d850f3aaa25171f163ed09608e
Daniel Porto <dankobaia@DESKTOP-1H15G9C.localdomain>
2019-12-26 21:26:38 -0300

    Updates

---
+++ precache-manifest.a147f134b7cd4f8c9d11e1b9ec26a9c2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4673f46c7fb868b75aaa3d2a4398ab6",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "ebce6818c7e3a49919ac",
+    "url": "/profile/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "3f66a2ee1a1e0af8b458",
+    "url": "/profile/static/css/main.8ae23e8f.chunk.css"
+  },
+  {
+    "revision": "ebce6818c7e3a49919ac",
+    "url": "/profile/static/js/2.275c6e9e.chunk.js"
+  },
+  {
+    "revision": "698ac75f55417f7a567ea092aaba4760",
+    "url": "/profile/static/js/2.275c6e9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f66a2ee1a1e0af8b458",
+    "url": "/profile/static/js/main.42358d40.chunk.js"
+  },
+  {
+    "revision": "828e100aa0104e8ae4d9",
+    "url": "/profile/static/js/runtime-main.08de336b.js"
+  },
+  {
+    "revision": "14ef890677220ede4909d06874309e3b",
+    "url": "/profile/static/media/FiraCode-Bold.14ef8906.woff2"
+  },
+  {
+    "revision": "7326289a1d2621d894b25f8992feb8d1",
+    "url": "/profile/static/media/FiraCode-Bold.7326289a.woff"
+  },
+  {
+    "revision": "816588e53597b0a335551f31f3346640",
+    "url": "/profile/static/media/FiraCode-Light.816588e5.woff"
+  },
+  {
+    "revision": "d9f533bdb76a46bae6eac67e4d63f087",
+    "url": "/profile/static/media/FiraCode-Light.d9f533bd.woff2"
+  },
+  {
+    "revision": "158022518e5552b1db695cfaae2127df",
+    "url": "/profile/static/media/FiraCode-Medium.15802251.woff"
+  },
+  {
+    "revision": "b1caf0571c4fd51df7aeff98bdc86df5",
+    "url": "/profile/static/media/FiraCode-Medium.b1caf057.woff2"
+  },
+  {
+    "revision": "0113c3ee6b94432a38db5c08a272b0d4",
+    "url": "/profile/static/media/FiraCode-Regular.0113c3ee.woff2"
+  },
+  {
+    "revision": "841b9e8ab26b928d0d14b21637bb625c",
+    "url": "/profile/static/media/FiraCode-Regular.841b9e8a.woff"
+  },
+  {
+    "revision": "dd9b4ad2e876efec29cf6c26e94cb4bf",
+    "url": "/profile/static/media/logo.dd9b4ad2.png"
+  }
+]);
